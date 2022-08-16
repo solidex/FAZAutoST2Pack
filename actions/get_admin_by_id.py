282233a0c4fe60b9e025b515bce06ed2e522572b
Matthew <draevich.matvey@gmail.com>
2022-08-16 13:33:45 +0300

    stackstorm modules are excluded from dependencies

--- actions/get_admin_by_id.py
+++ actions/get_admin_by_id.py
@@ -1,22 +1,26 @@
 import sys
-
-import sys
 import json
 import time
-
-from st2common.runners.base_action import Action
+import logging
 
 from st2client.client import Client
 from st2client.models import KeyValuePair
 
 DB_TTL = 600
 
-class GetAdminDataById(Action):
+class GetAdminDataById():
+
+    def __init__(self): 
+        #
+        # for testing purposes only
+        self.logger = logging.getLogger()
+
 
     def run(self, user_id):
 
-        client = Client(base_url='http://localhost')
-        cached_admin_info_kv = client.keys.get_by_name(KeyValuePair(name=user_id, scope='user'))
+        # client = Client(base_url='http://localhost')
+        # cached_admin_info_kv = client.keys.get_by_name(KeyValuePair(name=user_id, scope='user'))
+        cached_admin_info_kv = None
 
         admin_info = {
             'admin_name': '',
@@ -43,6 +47,15 @@ class GetAdminDataById(Action):
                 }
             self.logger.debug('admin_info returned from DB, %s' % admin_info)
 
-        client.keys.update(KeyValuePair(name=user_id, value=json.dumps(admin_info), ttl=DB_TTL, scope='user'))
+        # client.keys.update(KeyValuePair(name=user_id, value=json.dumps(admin_info), ttl=DB_TTL, scope='user'))
 
         return(True, admin_info)
+
+
+
+if __name__ == "__main__":
+    get_admin_data = GetAdminDataById()
+    #
+    result = get_admin_data.run(user_id="3752961995xx")
+    print(result)
+    #
