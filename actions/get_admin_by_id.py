8fd2c951bb2fe40c70981dd346082b77aa93ab95
Matthew <draevich.matvey@gmail.com>
2022-08-25 14:43:04 +0300

    is_testing flag is added for switching between test & production environments; get_new_alerts.py.run_test() is added for running in test environment; key_management_handler.py is removed because there is no need anymore

--- actions/get_admin_by_id.py
+++ actions/get_admin_by_id.py
@@ -5,11 +5,8 @@ import time
 import logging
 
 import oracledb as dbapi
-from st2client.client import Client
-from st2client.models import KeyValuePair
 from st2common.runners.base_action import Action
 
-from key_management_handler import CustomKeyManagementHandler
 
 
 DB_TTL = 600
@@ -23,8 +20,6 @@ def oracledb_thin_client(*args, logger, **kwargs):
     except: 
         logger.exception('Failed to connect to OracleDB using thin client!')
         return None
-
-
 
 
 def oracledb_thick_client(*args, logger, **kwargs):
@@ -48,53 +43,81 @@ def connect_to_oracledb(*args, **kwargs):
 
 
 
+
 class GetAdminDataById(Action):
 
+    def run_test(self, user_id):
+        admin_info = {
+            'admin_name': 'is_testing',
+            'admin_phone': user_id,
+            'admin_mail': 'admin@solidex.by',
+        }
+        self.logger.debug('admin_info returned from function GetAdminDataById.run_test, %s' % admin_info)
+        return admin_info
+
     def run(self, user_id):
-        keys = CustomKeyManagementHandler(base_url='http://localhost') 
+        # Check if we're working in testing environment
+        if self.config.get(is_testing, False):
+            self.logger.warning("activate testing environment for the reason is_testing={}".format(
+                                                                self.config.get(is_testing, False)))
+            return self.run_test(user_id=user_id)
         #
         admin_info = {
             'admin_name': '',
-            'admin_phone': '',
-            'admin_mail': ''
+            'admin_phone': user_id,
+            'admin_mail': '',
         }
         #
-        db_connect_str = keys.get_by_name(name="database_connect_str", scope='system').value
-        db_username = keys.get_by_name(name="database_username", scope='system').value
-        db_password = keys.get_by_name(name="database_password", scope='system', decrypt=True).value
-        db_query = keys.get_by_name(name="database_query", scope='system').value.format(user_id)
-        #
-        self.logger.debug('database connection string: {}'.format(db_connect_str))
-        self.logger.debug('database username/password: {}'.format(db_username+":"+db_password[:2]+"*********"))
-        self.logger.debug('database SQL query to execute: {}'.format(db_query))
-        #
-        #
-        #
-        cached_admin_info_kv = keys.get_by_name(name=user_id, scope='user')
+        cached_admin_info_kv = self.action_service.get_value(user_id)
         #
         if cached_admin_info_kv:
-            admin_info = json.loads(cached_admin_info_kv.value)
-            self.logger.debug('admin_info returned from cache, %s' % admin_info)
+            admin_info = json.loads(cached_admin_info_kv)
+            self.logger.info('admin_info returned from cache, %s' % admin_info)
         else:
+            #
+            # database access configuration
+            db_connect_str = self.config.get("db_connect_str", "<>")
+            db_username = self.config.get("db_username", "<>")
+            db_password = self.config.get("db_password", "<>")
+            db_query = self.config.get("db_query", "<>").format(user_id)
+            self.logger.debug('database connection string: {}'.format(db_connect_str))
+            self.logger.debug('database login/password: {}:{}'.format(db_username, db_password[:2] + 8 * "*"))
+            self.logger.debug('database SQL query to execute: \n{}'.format(db_query))
+            #
+            # connect & retrieve cursor from OracleDB
+            self.logger.info('Trying to connect to database...')
             cursor = connect_to_oracledb(dsn=db_connect_str, user=db_username, password=db_password, logger=self.logger)
-            if cursor is None:  # thin & thick clients seems to be non-working
+            if cursor is None:  # connection seems to be non-working
                 self.logger.error('Failed to connect to database!')
                 return (False, admin_info)
+            self.logger.info('Database connection & cursor were created!')
             #
-            self.logger.debug('Database connection & cursor were created!')
-            self.logger.debug('Executing SQL query on database...')
+            # executing SQL query
+            self.logger.info('Executing SQL query on database...')
+            try:
+                data = cursor.execute(db_query).fetchall()
+                self.logger.info('SQL query is successfully executed: {} result(s)'.format(len(data)))
+            except:
+                data = []
+                self.logger.exception('SQL query is failed')
             #
-
-
-
-            # admin_info["admin_phone"] = data_rows[0][0]
-            # admin_info["admin_name"] = data_rows[0][1]
-            # admin_info["admin_mail"] = data_rows[0][2]
-
+            # process SQL results
+            if len(data) == 0:
+                self.logger.warning('No results found for {}'.format(user_id))
+                return (True, admin_info)
+            if len(data) > 1:
+                self.logger.warning('Too many results found for {}').format(len(data), user_id)
+            self.logger.info('Extracting values from SQL results...')
+            try:
+                admin_info["admin_mail"] = data[0][0]
+                self.logger.info('SQL results are successfully extracted')
+            except:
+                self.logger.critical('Failed to extract values from SQL results: {}'.format(data[0]))
             #
-            # self.logger.debug('admin_info returned from DB, %s' % admin_info)
+            self.logger.debug('admin_info returned from database, %s' % admin_info)
+            self.logger.debug('admin_info has added to cache for user_id={} TTL={}'.format(user_id, DB_TTL))
+            self.action_service.set_value(name=user_id, value=json.dumps(admin_info), ttl=DB_TTL)
         #
-        keys.update(KeyValuePair(name=user_id, value=json.dumps(admin_info), ttl=DB_TTL, scope='user'))
         return (True, admin_info)
 
 
